6d7994862922f3c838d4cc40af638f7735470877
Yaps960 <yapan960@gmail.com>
2025-03-31 23:25:02 +0800

    � feat: Main script to run all segmentation tasks

---
+++ main.m
@@ -0,0 +1,10 @@
+%% Final Project
+% Developers (TN36):
+% Mercado, Raphael Andre
+% Yapan, Von Emil
+
+clear; clc; close all;
+
+% Starts the 
+imageFiles = data_preprocessing();
+fprintf('\nImage Processing Completed.\n');
